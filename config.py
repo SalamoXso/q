d6637560f57c14974a6486328d948b64b06f82fc
Salamo <subfornothing1994@gamil.com>
2025-11-28 01:06:04 +0100

    a

--- config.py
+++ config.py
@@ -5,14 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    # Use environment variables from Render
     CLIENT_KEY = os.getenv('TIKTOK_CLIENT_KEY')
     CLIENT_SECRET = os.getenv('TIKTOK_CLIENT_SECRET')
-    REDIRECT_URI = os.getenv('REDIRECT_URI')
+    REDIRECT_URI = os.getenv('REDIRECT_URI', 'https://q-hszm.onrender.com/callback')
     ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
     
     # Video settings
     VIDEO_SETTINGS = {
-        'max_duration': 180,  # 3 minutes max
+        'max_duration': 180,
         'supported_formats': ['.mp4', '.mov', '.avi'],
-        'max_file_size': 500 * 1024 * 1024  # 500MB
+        'max_file_size': 500 * 1024 * 1024
     }
